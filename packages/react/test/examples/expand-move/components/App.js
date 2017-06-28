9a51d2a2fe0a627823274d9b73a22f518b9aa95f
Michael Dougall <laheen@gmail.com>
2017-06-28 17:11:08 +1000

    Add min height to example

--- packages/react/test/examples/expand-move/components/App.js
+++ packages/react/test/examples/expand-move/components/App.js
@@ -47,7 +47,7 @@ export default class App extends React.Component {
 
   render () {
     return (
-      <div>
+      <div className="root">
         {this.state.big && <Container><BoxWithReverseTransition transitionPair="box-to-box" type="big" onClick={this.toggle} /></Container>}
 
         {this.state.big || <Container><BoxWithTransition transitionPair="box-to-box" type="small" onClick={this.toggle} className="float-right" /></Container>}
