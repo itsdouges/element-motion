90de5d2b6db64aad4fb8c52eeea4a9cb6accb252
Michael Dougall <laheen@gmail.com>
2017-07-02 19:07:32 +1000

    Tweak docs

--- scripts/buildDocs.sh
+++ scripts/buildDocs.sh
@@ -3,3 +3,4 @@ mkdir ./docs
 npm run build
 cp -R ./packages/react/dist/* ./docs
 cp -R ./packages/react/test/examples/dark-side/* ./docs
+cp ./packages/react/node_modules/normalize.css/normalize.css ./docs/normalize.css
