f8b1d018910a42de5cfb3f04ff4068117633ef3e
Michael Dougall <laheen@gmail.com>
2017-07-04 15:32:09 +1000

    Start imlpementing multi points

--- packages/react/test/examples/dark-side/components/Box.js
+++ packages/react/test/examples/dark-side/components/Box.js
@@ -2,9 +2,9 @@
 
 import React from 'react';
 
-const Box = ({ type, className, src, ...props }: { className?: string, type: 'big' | 'small', src: string }) => (
+const Box = ({ type, className, src, onClick }: { className?: string, type: 'big' | 'small', src: string }) => (
   <div
-    {...props}
+    onClick={onClick}
     className={`box box-${type}${className ? ` ${className}` : ''}`}
     // $FlowFixMe - Dynamic import
     style={{ backgroundImage: `url(${require(`../images/${src}`)})` }}
