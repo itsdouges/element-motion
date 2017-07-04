bb0d17cd4c0ab4056e0252af2cd2ae25b931aeba
Michael Dougall <laheen@gmail.com>
2017-07-04 15:32:09 +1000

    Start imlpementing multi points

--- packages/react/test/examples/dark-side/components/App.js
+++ packages/react/test/examples/dark-side/components/App.js
@@ -23,10 +23,41 @@ const BoxWithTransition = withTransition([{
   matchSize: true,
 }])(Box);
 
+const items = [{
+  name: 'First Order',
+  src: 'first-order.jpg',
+  description: (
+    <content>
+      <p>
+        {"The First Order, simply referred to as the Order, was a political and military faction—ruled by Supreme Leader Snoke and allied with the Knights of Ren—that came into existence as a result of the fall of the Galactic Empire. In the aftermath of the Battle of Jakku, the Empire formally surrendered to the New Republic by signing the Galactic Concordance, a peace treaty that marked the end of the Galactic Civil War in 5 ABY. In spite of its collapse the Empire's legacy survived in the Unknown Regions of the galaxy where former members of the Imperial Military plotted their return to power. Their cause would grow in support through some Imperial sympathizers who briefly inhabited a wing of Republic politics before abandoning the nascent democracy to join the Imperial-based movement in the Unknown Regions. Inspired by the fascist ideals of the Empire, this movement ultimately resulted in the formation of the First Order."}
+      </p>
+
+      <p>
+        {"In the decades that followed the Empire's defeat, the First Order gradually built its strength through the secret mobilization of new fleets and armies in violation of the Galactic Concordance and Republic law. Despite further acts of defiance to the treaty, the Galactic Senate did not regard the First Order as a serious threat to the Republic. However, the Senate's inaction motivated the Rebel veteran Princess Leia Organa to found the Resistance. Although the First Order became entangled in a conflict with Organa's group, the Republic remained its primary target. After thirty years of plotting their revenge against the government that overthrew the Empire, the First Order test-fired the superweapon of Starkiller Base on the Hosnian system and thereby destroyed Hosnian Prime, the capital world of the Republic. Shortly thereafter, the Resistance launched a counterattack that resulted in the base's destruction."}
+      </p>
+    </content>
+  ),
+}, {
+  name: 'Emporer',
+  src: 'emporer.jpg',
+}, {
+  name: 'Darth Maul',
+  src: 'darth-maul.jpg',
+}, {
+  name: 'Boba Fett',
+  src: 'boba-fett.jpg',
+}, {
+  name: 'Ben Kenobi',
+  src: 'ben.jpeg',
+}, {
+  name: 'Captain Phasma',
+  src: 'phasma.jpg',
+}];
+
 export default class App extends React.Component {
   state = {
     big: false,
-    src: 'first-order.jpg',
+    item: items[0],
   };
 
   componentDidMount () {
@@ -39,10 +70,10 @@ export default class App extends React.Component {
     });
   }
 
-  toggle = (src: string) => {
+  select = (item: any) => {
     this.setState((prevState) => ({
       big: !prevState.big,
-      src,
+      item,
     }));
   };
 
@@ -51,7 +82,7 @@ export default class App extends React.Component {
       <div className="root">
         {this.state.big && (
           <Container>
-            <BoxWithContent onClick={this.toggle} src={this.state.src} />
+            <BoxWithContent onClick={this.toggle} {...this.state.item} />
           </Container>
         )}
 
@@ -60,20 +91,16 @@ export default class App extends React.Component {
             <img alt="Empire Insignia" src={`${require('../images/logo.png')}`} className="insignia" />
 
             <div className="container-inner">
-              <Box type="small" src="ben.jpeg" />
-              <Box type="small" src="darth-maul.jpg" />
-              <Box type="small" src="boba-fett.jpg" />
-
-              <BoxWithTransition
-                transitionPair="box-to-box"
-                type="small"
-                onClick={() => this.toggle('first-order.jpg')}
-                src="first-order.jpg"
-                className="box-highlighted"
-              />
-
-              <Box type="small" src="emporer.jpg" />
-              <Box type="small" src="phasma.jpg" />
+              {items.map((item) => (
+                <BoxWithTransition
+                  {...item}
+                  key={item.name}
+                  type="small"
+                  className="box-highlighted"
+                  transitionPair={item.name}
+                  onClick={() => this.select(item)}
+                />
+              ))}
             </div>
 
             <div className="vader-container">
