006758abf42fd9d09c38dc137cf6a85f7880a85b
Michael Dougall <laheen@gmail.com>
2017-07-10 22:27:09 +1000

    Add dynamic background colours for test page, extract data out

--- packages/react/test/examples/dark-side/components/BoxWithContent.js
+++ packages/react/test/examples/dark-side/components/BoxWithContent.js
@@ -3,22 +3,10 @@
 import React from 'react';
 import Box from './Box';
 import TransitionContainer from '../../../../src/TransitionContainer';
-import withTransition from '../../../../src/withTransition';
+import Transition from '../../../../src/Transition';
 
-const BoxWithReverseTransition = withTransition([{
-  transition: 'expand',
-  duration: 0.3,
-  background: '#782128',
-  reverse: true,
-  cover: true,
-}, {
-  transition: 'move',
-  duration: 0.4,
-  matchSize: true,
-}])(Box);
-
-const BoxWithContent = ({ onClick, src, description, name }: any) => (
-  <TransitionContainer pair={name} className="content-bg">
+const BoxWithContent = ({ onClick, src, description, name, color }: any) => (
+  <TransitionContainer pair={name} className="content-bg" style={{ backgroundColor: color }}>
     <div className="content-text">
       {name.toUpperCase().split('').map((char, index) =>
         // eslint-disable-next-line react/no-array-index-key
@@ -26,13 +14,28 @@ const BoxWithContent = ({ onClick, src, description, name }: any) => (
     </div>
 
     <div className="content-margin">
-      <BoxWithReverseTransition
-        className="box-highlighted"
-        transitionPair={name}
-        type="big"
-        onClick={onClick}
-        src={src}
-      />
+      <Transition
+        pair={name}
+        transitions={[{
+          transition: 'expand',
+          duration: 0.3,
+          background: color,
+          reverse: true,
+          cover: true,
+        }, {
+          transition: 'move',
+          duration: 0.4,
+          matchSize: true,
+        }]}
+      >
+        <Box
+          className="box-highlighted"
+          transitionPair={name}
+          type="big"
+          onClick={onClick}
+          src={src}
+        />
+      </Transition>
 
       {description}
     </div>
