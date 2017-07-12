b1f18cd8aeb4cbad35dd4c5fb79182dbff18a1fc
Michael Dougall <laheen@gmail.com>
2017-07-12 19:59:50 +1000

    Fix test

--- packages/core/src/transitions/index.spec.js
+++ packages/core/src/transitions/index.spec.js
@@ -2,6 +2,6 @@ import * as module from './';
 
 describe('core root', () => {
   it('should pass', () => {
-    expect(module).to.have.keys('expand', 'fadeout', 'move', 'reveal');
+    expect(module).to.have.keys('circleExpand', 'circleShrink', 'fadeout', 'move', 'reveal', 'custom');
   });
 });
