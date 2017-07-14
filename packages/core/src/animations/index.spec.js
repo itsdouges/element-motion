31b2eb0519304cdc31aab0ccfddac67a987c02f0
Michael Dougall <laheen@gmail.com>
2017-07-14 10:01:34 +1000

    Fix build

--- packages/core/src/animations/index.spec.js
+++ packages/core/src/animations/index.spec.js
@@ -2,6 +2,6 @@ import * as module from './';
 
 describe('core root', () => {
   it('should pass', () => {
-    expect(module).to.have.keys('circleExpand', 'circleShrink', 'fadeout', 'move', 'reveal', 'custom');
+    expect(module).to.have.keys('circleExpand', 'circleShrink', 'fadeout', 'move', 'reveal', 'custom', 'swipe');
   });
 });
