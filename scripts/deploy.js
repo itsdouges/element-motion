fafb34804c78e7fded792c13c5334c94263f023a
Michael Dougall <laheen@gmail.com>
2017-07-28 08:41:58 +1000

    chore: corrects deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,12 +4,13 @@ const { version } = require('../package.json');
 const { exec } = require('child_process');
 
 exec(`lerna publish --yes --skip-npm --skip-git --force-publish=* --repo-version=${version}`, (error, stdout, stderr) => {
-  if (error || stderr) {
-    console.error(error || stderr);
+  if (error) {
+    console.error(error);
     process.exit(1);
     return;
   }
 
+  console.log(strerr);
   console.log(stdout);
   process.exit(0);
 });
