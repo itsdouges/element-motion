58e668cab89f2123600c17876de0a3f872fd4edf
madou <laheen@gmail.com>
2019-01-19 18:18:15 +1100

    hmm

--- packages/yubaba/src/lib/move.tsx
+++ packages/yubaba/src/lib/move.tsx
@@ -6,6 +6,8 @@ interface CoverOpts {
   transformY?: boolean;
 }
 
+type Point = [number, number];
+
 export function cover(
   origin: ElementBoundingBox,
   destination: ElementBoundingBox,
@@ -20,6 +22,15 @@ export function cover(
   };
 }
 
+const porabola = (vertex: Point, point: Point) => (percent: number): Point => {
+  const [x0, y0] = vertex;
+  const [x1, y1] = point;
+  const y = ((y0 - y1) / (x0 - x1)) * (1 - percent / 100) + y1;
+  const x = percent * ((x1 - x0) / 100) + x0;
+  console.log(percent, [x, y]);
+  return [x, y];
+};
+
 export function arcLeft(
   origin: ElementBoundingBox,
   destination: ElementBoundingBox,
@@ -27,20 +38,20 @@ export function arcLeft(
 ): string {
   const translateX = origin.location.left - destination.location.left;
   const translateY = origin.location.top - destination.location.top;
-  const scaleX = origin.size.width / destination.size.width;
-  const scaleY = origin.size.height / destination.size.height;
+  const vertex: Point = [origin.location.left, origin.location.top];
+  const point: Point = [destination.location.left, destination.location.top];
+
   const kframes = keyframes`
-      0% {
-        transform: translate3d(${translateX}px, ${translateY}px, 0) scale3d(${scaleX}, ${scaleY}, 1);
-      }
-
-      50% {
-        transform: translate3d(${translateX}px, 0, 0) scale3d(1, 1, 1);
-      }
-
-      100% {
-        transform: translate3d(0, 0, 0) scale3d(1, 1, 1);
-      }
+      ${Array(100)
+        .fill(undefined)
+        .map((_, percent) => {
+          const [x, y] = porabola(vertex, point)(percent);
+          return `
+        ${percent}% {
+        transform: translate3d(${x}px, ${y}px, 0);
+      }`;
+        })
+        .join('')}
   `;
 
   return css`
