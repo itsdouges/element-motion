cd6e4afc4a43a95e9bc5ce205fa337fde6d06575
Michael Dougall <mdougall@atlassian.com>
2019-05-10 15:59:59 +1000

    chore(test-components): adds interval logic to toggler

--- packages/yubaba-common/src/Toggler.tsx
+++ packages/yubaba-common/src/Toggler.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 
 interface Props {
-  children: (
-    props: {
-      toggle: (value?: string) => void;
-      set: (value: string) => void;
-      shown: boolean | string;
-    }
-  ) => React.ReactNode;
+  interval: boolean;
+  intervalMs: number;
+  onIntervalSet: (value: string | boolean) => string | boolean;
+  children: (props: {
+    toggle: (value?: string) => void;
+    set: (value: string) => void;
+    shown: boolean | string;
+  }) => React.ReactNode;
 }
 
 interface State {
@@ -15,17 +16,46 @@ interface State {
 }
 
 export default class Toggler extends React.Component<Props, State> {
+  static defaultProps = {
+    interval: false,
+    intervalMs: 3000,
+    onIntervalSet: () => {},
+  };
+
   state = {
     shown: false,
   };
 
+  cleanup: () => void = () => {};
+
+  componentDidMount() {
+    if (this.props.interval) {
+      const intervalId = window.setInterval(() => {
+        const result = this.props.onIntervalSet(this.state.shown);
+        this.setState(prevState => ({
+          shown: result !== undefined ? result : !prevState.shown,
+        }));
+      }, this.props.intervalMs);
+
+      this.cleanup = () => window.clearInterval(intervalId);
+    }
+  }
+
+  componentWillUnmount() {
+    this.cleanup();
+  }
+
   toggle = (value?: string) => {
+    this.cleanup();
+
     this.setState(prevState => ({
       shown: prevState.shown ? false : value || true,
     }));
   };
 
   set = (value: string) => {
+    this.cleanup();
+
     this.setState({
       shown: value,
     });
