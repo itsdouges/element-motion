85b65af5bb68a49136d2bd2237462dcad027ec1f
Michael Dougall <mdougall@atlassian.com>
2019-05-10 16:01:54 +1000

    chore(docs): adds facebook messenger notification pill example

--- packages/yubaba/src/__docs__/2-getting-started/styled.tsx
+++ packages/yubaba/src/__docs__/2-getting-started/styled.tsx
@@ -296,3 +296,51 @@ export const data = [
     description: 'Need some more Slacks?',
   },
 ];
+
+const avatarSize = '18px';
+
+/**
+ * Message notification styled
+ */
+export const MessageNotification = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  font-weight: 600;
+  width: 100%;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  cursor: pointer;
+`;
+
+export const AlignCenter = styled.div`
+  text-align: center;
+`;
+
+export const NotificationText = styled.span`
+  width: calc(100% - ${avatarSize});
+  padding-right: 12px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+export const Avatar = styled.img`
+  border-radius: 50%;
+  height: ${avatarSize};
+  width: ${avatarSize};
+  flex-shrink: 0;
+  margin-right: 6px;
+  object-fit: cover;
+  background-color: #ccc;
+`;
+
+export const messages = (index: number = 0) => {
+  const msgs = [
+    'seriously help me i cant get past genchrio',
+    'sekiro is so hard meh',
+    'hello???',
+    'how do i cheese it????',
+  ];
+  return msgs[index % msgs.length];
+};
